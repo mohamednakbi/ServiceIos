1e90547c3f4eab59add256d2efd97b1d6ec521c9
mohamednakbi <mohamed.nakbi@esprit.tn>
2022-04-27 01:28:44 +0100

    first commit

---
+++ keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+	MONGOURI: "mongodb+srv://mohamed:<nakbi>@cluster0.hgvdq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+	JWT_SECRET:"effegwegwrgrgw4"
+
+
+	//mongodb+srv://mohamed:<nakbi>@cluster0.hgvdq.mongodb.net/mohamed?retryWrites=true&w=majority
+
+
+}	
---
+++ service/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	MONGOURI: "mongodb+srv://paradax:plBxclxd4TI4i0VO@cluster0.8r9tq.mongodb.net/test",
+	JWT_SECRET:"effegwegwrgrgw4"
+}
