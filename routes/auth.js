d8b7fbf691183d7a05447b60095b7d3d8f777450
Mohamed Nakbi <73251097+mohamednakbi@users.noreply.github.com>
2022-06-27 13:44:22 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -148,12 +148,12 @@ router.post("/forgetPassword", async (req, res) => {
         message: "email not found",
       });
     } else {
-     
+     /*
         var code = (Math.floor(Math.random() * 10000) + 10000)
         .toString()
         .substring(1);
       console.log(code);
-     /**/
+     */
       var code = Math.floor(Math.random() * 100000);
       var mailContent = `Almost done : ` + code;
   
